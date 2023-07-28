1202ce8221587ebac868b3fe9c8a4be2eac0eba5
Meyzan Al Yutra <60688967+Meyzan16@users.noreply.github.com>
2023-07-28 18:39:59 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,12 +10,17 @@ import absenRoutes from './routes/absen.js';
 
 import cors from 'cors';
 import path from 'path';
+import {fileURLToPath} from 'url';
 
 //configure env
 dotenv.config();
 
 //database
 connectToDB();
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
